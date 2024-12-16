2c87c2f8a0f8609b44e3e03c479470d445b6ce89
Syed Safdar Ali Shah <programmersafdar@live.com>
2024-12-16 21:44:35 +0500

    Team Agentic -> 3 Agents created and working amazing
    Signed-off-by: Syed Safdar Ali Shah <programmersafdar@live.com>

--- TeamsAgents2/main.py
+++ TeamsAgents2/main.py
@@ -2,18 +2,34 @@ from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.model.openai import OpenAIChat
 from dotenv import load_dotenv
+from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 load_dotenv()
-if __name__ == '__main__':
-    agent = Agent(
-        # model=Groq(id='llama-3.3-70b-versatile'),
-        model=OpenAIChat(id="gpt-3.5-turbo"),
-        # model=OpenAIChat(id="gpt-4"),
-        # model=OpenAIChat(id="gpt-4o"),
+
+WebAgent = Agent(
+        name='Web Agent',
+        model=Groq(id='llama-3.3-70b-versatile'),
+        tools=[DuckDuckGo()],
+        instructions=["Always include sources"],
+        show_tool_calls=True,
+        markdown=True
+
+    )
+FinanceAgent = Agent(
+        name="Finance Agent",
+        model=Groq(id='llama-3.3-70b-versatile'),
         tools=[YFinanceTools(stock_price=True, analyst_recommendations=True,stock_fundamentals=True)],
         show_tool_calls=True,
         instructions=['use tables to display data..',
                       'if you dont know the company symbol, please use get_compony_symbol tool. even if it is not a public company'],
         markdown=True
                   )
-    agent.print_response('Summarize and compare analyst recommendation and fundamentals for TSLA and Phidata ')
+AgentTeam = Agent(
+        team=[WebAgent,FinanceAgent],
+        model=Groq(id='llama-3.3-70b-versatile'),
+        instructions=["Always include source","Use tables to display data"],
+        show_tool_calls=True,
+        markdown=True
+    )
+if __name__ == '__main__':
+    AgentTeam.print_response('Summarize analyst recommendation and share the latest news for NVDA ', stream=True)
