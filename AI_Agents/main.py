45059aac5896455e76b4065c9b37c9d6f19b0663
Syed Safdar Ali Shah <programmersafdar@live.com>
2024-12-16 11:43:01 +0500

    Init

---
+++ AI_Agents/main.py
@@ -0,0 +1,6 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+if __name__ == '__main__':
+    agent = Agent(model=Groq(id='llama-3.3-70b-versatile'))
